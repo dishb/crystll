673b7941c7731471d0b0dc804e919140297d65c2
dishb <code.dishb@gmail.com>
2025-05-31 18:21:51 -0700

    feat: finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Upload, MousePointerClick, History } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
@@ -12,12 +13,19 @@ export default function Page() {
     <div className="-mt-16 flex flex-col items-center">
       <div className="h-screen flex justify-center items-center w-350">
         <div className="w-1/2 flex flex-col justify-center items-start">
-          <h2 className="text-4xl">
-            Upload. Click. <span className="font-medium text-grape">Done.</span>
-          </h2>
-          <h1 className="text-9xl mt-4">
-            Yes, it&apos;s <span className="italic text-ocean">that</span> easy.
-          </h1>
+          <div className="flex flex-col w-full h-full">
+            <h2 className="text-4xl">
+              Upload. Click.{" "}
+              <span className="font-medium text-grape">Done.</span>
+            </h2>
+            <h1 className="text-9xl mt-4">
+              Yes, it&apos;s <span className="italic text-ocean">that</span>{" "}
+              easy.
+            </h1>
+          </div>
+          <h3 className="text-3xl mt-20">
+            Take the first and final step towards better finance management.
+          </h3>
         </div>
 
         <Image
@@ -29,8 +37,57 @@ export default function Page() {
         />
       </div>
 
-      <div className="w-200 flex flex-col">
-        <h2 className="text-3xl font-medium text-ocean">FAQs</h2>
+      <hr className="w-full border-1 my-30" />
+
+      <div className="w-full px-30 flex flex-col gap-50">
+        <p className="text-4xl text-center">
+          Club finance management is{" "}
+          <span className="text-grape italic">flawed</span>. Many clubs
+          don&apos;t even track their purchases in the first place.
+          <br /> <br />
+          At <span className="text-ocean">crystll</span>, we&apos;re using
+          software to develop the perfect solution.
+        </p>
+      </div>
+
+      <div className="w-full flex justify-around items-center px-30 mt-60">
+        <div className="flex flex-col justify-center items-center">
+          <Upload size="5.5rem" className="text-grape" />
+          <h3 className="mt-5 text-5xl text-grape">1. Upload a receipt</h3>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <MousePointerClick size="5.5rem" className="text-ocean" />
+          <h3 className="mt-5 text-5xl text-ocean">2. Click to submit</h3>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <History size="5.5rem" className="text-blue" />
+          <h3 className="mt-5 text-5xl text-blue">3. View purchase log</h3>
+        </div>
+      </div>
+
+      <hr className="w-full border-1 my-30" />
+
+      <div className="flex flex-col justify-center px-30 max-w-200 gap-4">
+        <h2 className="text-5xl">What is crystll?</h2>
+        <h3 className="text-3xl text-blue">
+          It's a finance tracker for school clubs.
+        </h3>
+        <p className="text-2xl">
+          To use crystll, you simply upload an image of a receipt or invoice
+          that contains details of your purchase(s).
+          <br /> <br />
+          We then extract important information, like the merchant and total
+          cost, and finally log the receipt information. Users can then view
+          their club's entire purchase history.
+        </p>
+      </div>
+
+      <hr className="w-full border-1 my-30" />
+
+      <div className="w-full px-30 flex flex-col">
+        <h2 className="text-4xl font-medium text-ocean">FAQs</h2>
 
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
@@ -95,6 +152,21 @@ export default function Page() {
               formatting, etc. All of these different things take a lot of
               research and time, neither of which are worth it. Google Sheets
               also isn't scalable. Be simple and use crystll.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-6">
+            <AccordionTrigger>
+              What if I still have more questions?
+            </AccordionTrigger>
+            <AccordionContent>
+              Contact us{" "}
+              <Link
+                className="text-ocean underline underline-offset-4  hover:no-underline"
+                href="/contact"
+              >
+                here
+              </Link>
+              !
             </AccordionContent>
           </AccordionItem>
         </Accordion>
