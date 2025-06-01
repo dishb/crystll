4edd58fba94aeaf884b3d1d31a4668175185c93b
dishb <code.dishb@gmail.com>
2025-05-31 22:11:31 -0700

    fix: ESLint errors again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next"],
+    rules: {},
+  }),
+];
+
+export default eslintConfig;
