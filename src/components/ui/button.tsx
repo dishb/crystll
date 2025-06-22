a8e07b5a859a906de7c96c23e15e990735bb4bd8
dishb <code.dishb@gmail.com>
2025-06-22 10:43:46 -0700

    edit: change hover effect on buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,9 +15,9 @@ const buttonVariants = cva(
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         grape:
-          "border text-grape border-grape bg-background shadow-xs hover:bg-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border text-grape border-grape bg-background shadow-xs hover:bg-grape hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         ocean:
-          "border text-ocean border-ocean bg-background shadow-xs hover:bg-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border text-ocean border-ocean bg-background shadow-xs hover:bg-ocean hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
